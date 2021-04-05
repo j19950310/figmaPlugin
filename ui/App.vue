cfae8cf66233beb685d229bb0c5cdb1451c51064
Jay <jaywu.fe@gmail.com>
2021-04-05 18:11:55 +0800

    First upload

---
+++ ui/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+        app
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+    #app {
+        font-size: 100px;
+    }
+</style>
---
+++ ui/main.js
@@ -0,0 +1,8 @@
+import './src/style/_main.scss'
+import Vue from 'vue'
+import App from './App'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
