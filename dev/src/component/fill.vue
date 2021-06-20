ead8ea377e3c9f451762b8acf4b04226fdc952a2
Jay <jaywu.fe@gmail.com>
2021-06-21 01:07:46 +0800

    [feat] 組件 - 相片格式

---
+++ dev/src/component/fill.vue
@@ -0,0 +1,46 @@
+<template>
+    <img class="fill" 
+        :src="src" 
+        @load="onLoad" 
+        alt="">
+</template>
+<script>
+export default {
+    props: {
+        content: {
+            type: Uint8Array
+        },
+        max: {
+            type: Number,
+            default: 150
+        }
+    },
+    data() {
+        return {
+            src: '',
+        }
+    },
+    mounted() {
+        this.src = URL.createObjectURL(new Blob([this.content.buffer], { type: 'image/png' }))
+    },
+    methods: {
+        onLoad(e) {
+            // this.$nextTick(()=>{
+            //     const height = this.$el.height
+            //     const width = this.$el.width
+            //     const MAX = this.max
+            //     const ratio = MAX / Math.max(width, height)
+            //     this.$el.width = ratio * width
+            //     this.$el.height =ratio * height
+            // })
+        }
+    },
+}
+</script>
+<style lang="scss">
+.fill {
+    display: block;
+    width: auto;
+    height: 75px;
+}
+</style>
---
+++ dev/src/component/image.vue
@@ -0,0 +1,39 @@
+<template>
+    <img class="image" 
+        :src="content" 
+        @load="onLoad" 
+        alt="">
+</template>
+<script>
+export default {
+    props: {
+        content: {
+            type: String,
+        },
+        max: {
+            type: Number,
+            default: 150
+        }
+    },
+    data() {
+        return {
+            src: '',
+        }
+    },
+    mounted() {
+    },
+    methods: {
+        onLoad(e) {
+            
+        },
+    },
+}
+</script>
+<style lang="scss">
+.image {
+    // @include size(100px);
+    display: block;
+    width: auto;
+    height: 75px;
+}
+</style>
